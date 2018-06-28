e78cb9bed97b9d4d99c9228db777d7cced5ecf7d
Clau1889 <clau.blanco1889@gmail.com>
2018-06-27 21:31:52 -0500

    Se reacomoda barra de navegacion

--- js/app.js
+++ js/app.js
@@ -16,22 +16,22 @@ $(document).ready(function(){
         }
     });
 
-    document.getElementById("logout").addEventListener('click',function (){
-    //console.log('click');
-    firebase.auth().signOut();
-    });
+    // document.getElementById("logout").addEventListener('click',function (){
+    // //console.log('click');
+    // firebase.auth().signOut();
+    // });
 
 });
 
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      //console.log(user);
-      //console.log(user.displayName);
-      //console.log(user.photoURL);
-      //console.log(user.uid)
-    } else {
-      console.log('desloggeado');
-      window.location="index.html";
-    }
-});
+// firebase.auth().onAuthStateChanged(function(user) {
+//     if (user) {
+//       //console.log(user);
+//       //console.log(user.displayName);
+//       //console.log(user.photoURL);
+//       //console.log(user.uid)
+//     } else {
+//       console.log('desloggeado');
+//       window.location="index.html";
+//     }
+// });
 
