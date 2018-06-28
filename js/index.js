d6a3ca9e68921d60be67c461ffb4cf79a83c7c4d
latilanoh@gmail.com <latilanoh@gmail.com>
2018-06-27 21:42:19 -0500

    se crea la modal del sign up y pruebas con la funcionalidad de los botones

--- js/index.js
+++ js/index.js
@@ -4,37 +4,50 @@ $(document).ready(function () {
     // setTimeout(function () {
     //     $('#splash').fadeOut(500);
     // }, 1000);
+});    
 
-    document.getElementById("btn-login").addEventListener('click',function (){
-        //console.log('click');
-        var provider = new firebase.auth.GoogleAuthProvider();
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {opacity: 0.8});
+});*/
 
-        firebase.auth().signInWithPopup(provider)
-        .then(function(result){})
-        .catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-        });
+document.getElementById("btn-signup").addEventListener('click',function(){
+    //console.log("sign up");
+    $('#modal-SignUp').modal('show');
+});
 
-    });
-
-    firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-        window.location="app.html";
-        } else {
-        console.log('desloggeado');
-        }
-    });
-
-    document.getElementById("btn-logout").addEventListener('click',function (){
-        //console.log('click');
-        firebase.auth().signOut();
-    });
+document.getElementById("btn-login").addEventListener('click',function(){
+    console.log('login');
+    /*var provider = new firebase.auth.GoogleAuthProvider();*/
+    
+    /* firebase.auth().signInWithPopup(provider)
+    .then(function(result){
+        saveUser(result.user);
+    })
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });*/
 
 });
+
+document.getElementById("btn-logout").addEventListener('click',function (){
+    console.log('logout');
+    //firebase.auth().signOut();
+});
+
+/*firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+    window.location="app.html";
+    } else {
+    console.log('desloggeado');
+    }
+});*/
+
